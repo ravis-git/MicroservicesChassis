09815c9a15e7fa17d07a3adcc4af676e6a910d4c
Ravi Palakodeti <ravi.palakodeti@ge.com>
2017-08-23 14:12:44 -0500

    removued unused dependencies

--- src/main/groovy/com/my/experiments/microservices/chassis/plugins/ChassisPlugin.groovy
+++ src/main/groovy/com/my/experiments/microservices/chassis/plugins/ChassisPlugin.groovy
@@ -31,32 +31,13 @@ class ChassisPlugin implements Plugin<Project> {
         project.apply plugin: GroovyPlugin
         project.apply plugin: 'org.springframework.boot' // requried dependency was added in build.gradle
 
-        // add all other common dependencies for all microservices
-
-//        addJerseyDependencies(project)
-//        addLombokDependencies(project)
+        // add plugin application aa compile/runtime dependency to use plugin functions
         addPluginAsDependency(project)
-//        addConsulDependencies(project)
     }
-
-/*    private addJerseyDependencies(Project project) {
-        log.info " ========== loading spring boot version: ${springBootVersion} == "
-        project.dependencies.add('compile', 'org.springframework.boot:spring-boot-starter-jersey:1.5.6.RELEASE')
-    }*/
-
-
-/*    private static void addLombokDependencies(Project project) {
-        project.dependencies.add('compile', 'org.projectlombok:lombok:1.16.10')
-    }*/
 
     // add self as a dependency to all microservices to use the dependencies compile and runtime
     private addPluginAsDependency(Project project) {
         project.dependencies.add('compile',
                 'com.my.experiments.microservices.chassis.plugins:MicroservicesChassis:0.0.1-SNAPSHOT')
     }
-
-//    private addConsulDependencies(Project project) {
-//        project.dependencies.add('compile',
-//                'org.springframework.cloud:spring-cloud-starter-consul-discovery:1.2.1.RELEASE')
-//    }
 }
